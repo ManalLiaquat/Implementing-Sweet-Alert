b63ee0b2f0bcc7905495297ccd79034ca93572be
ManalLiaquat <m.manalliaquat@gmail.com>
2018-06-01 03:18:02 +0500

    using pre-defined functions of sweet alert

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function signup() {
+    swal({
+        title: "Success!",
+        text: "You successfully sign up!",
+        icon: "success",
+        button: "Ok!",
+    });
+}
+
+function signin() {
+    swal({
+        title: "Failed!",
+        text: "Error generate during sign in!",
+        icon: "error",
+        button: "Ok!",
+    });
+}
